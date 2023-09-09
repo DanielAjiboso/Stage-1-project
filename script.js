a5bc6a1754ee2c60a57e7b7cbf6de39b71269254
DanielAjiboso <danielajiboso@gmail.com>
2023-09-09 02:36:41 +0100

    final final commit

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 const nameOfDay = document.querySelector(".day-of-week");
 const utcTime = document.querySelector(".time");
 
-const present = new Date();
+setInterval(() => {
+  const present = new Date();
+  const dayOfWeek = present.toLocaleString("default", {
+    weekday: "long",
+  });
 
-const dayOfWeek = present.toLocaleString("default", {
-  weekday: "long",
-});
+  const doubleFigure = function (n) {
+    return ("0" + n).slice(-2);
+  };
 
-const doubleFigure = function (n) {
-  return ("0" + n).slice(-2);
-};
-
-const timeUTC = `${doubleFigure(present.getUTCHours())}:${doubleFigure(
-  present.getUTCMinutes()
-)}:${doubleFigure(present.getSeconds())}:${present.getUTCMilliseconds()}`;
-utcTime.innerHTML = timeUTC;
-
-nameOfDay.innerHTML = dayOfWeek;
+  const hrs = doubleFigure(present.getUTCHours());
+  const min = doubleFigure(present.getUTCMinutes());
+  const sec = doubleFigure(present.getSeconds());
+  const milliSec = present.getUTCMilliseconds();
+  const timeUTC = `${hrs}:${min}:${sec}:${milliSec}`;
+  utcTime.innerHTML = timeUTC;
+  nameOfDay.innerHTML = dayOfWeek;
+}, 1);
